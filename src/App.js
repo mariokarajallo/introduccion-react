2eda4ed85d3913751427494d163314668cf858c3
mariokarajallo <mario_karajallo@hotmail.com>
2022-10-03 13:57:15 -0300

    create components TodoCounter.jsx

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
+import React from "react";
+import { TodoCounter } from "./TodoCounter";
 // import './App.css';
 // lista falsa de TODOs
-const todos=[
-  {text:'cortar cebolla',completed: false},
-  {text:'tomar el curso',completed: false},
-  {text:'hcaer ejercicios',completed: false},
-]
+const todos = [
+  { text: "cortar cebolla", completed: false },
+  { text: "tomar el curso", completed: false },
+  { text: "hcaer ejercicios", completed: false },
+];
 function App() {
-return (
-  // todo lo que vamos a mostrar en nuestra aplicacion
+  return (
+    // todo lo que vamos a mostrar en nuestra aplicacion
+    <React.Fragment>
+      {/* // cuantos TODOs completados y creados */}
       <TodoCounter />
-      // cuantos TODOs completados y creados
-      <h2> Has comletado 2 de 3 TODOs</h2>
-      
-      <TodoSearch />
+
+      {/* <TodoSearch /> */}
       <input placeholder="Cebolla"></input>
-      
-      // contenedor de TODOs
-      <TodoList>
-        {/* enviar cada uno de los todo que necesitamos */}
-        {/* iteramos los elementos del array todos */}
-        {todos.map(todo => (
-          <TodoItem />
-        ))}
-      </TodoList>
-      
-      // boton que abre el modal para crear nuevos TODO
-      <CreateTodoButton />
+      {/* // contenedor de TODOs */}
+      {/* <TodoList> */}
+      {/* enviar cada uno de los todo que necesitamos */}
+      {/* iteramos los elementos del array todos */}
+      {/* {todos.map((todo) => ( */}
+      {/* <TodoItem /> */}
+      {/* ))} */}
+      {/* </TodoList> */}
+      {/* boton que abre el modal para crear nuevos TODO */}
+      {/* <CreateTodoButton /> */}
       <button>+</button>
+    </React.Fragment>
   );
 }
 
