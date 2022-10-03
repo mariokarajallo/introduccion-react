b6763ee8aec8835fb1dda48d5e38441a5b09e9ca
mariokarajallo <mario_karajallo@hotmail.com>
2022-10-02 23:33:31 -0300

    add components -> Apps.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 // import './App.css';
-
-functionApp() {
+// lista falsa de TODOs
+const todos=[
+  {text:'cortar cebolla',completed: false},
+  {text:'tomar el curso',completed: false},
+  {text:'hcaer ejercicios',completed: false},
+]
+function App() {
 return (
+  // todo lo que vamos a mostrar en nuestra aplicacion
       <TodoCounter />
+      // cuantos TODOs completados y creados
+      <h2> Has comletado 2 de 3 TODOs</h2>
+      
       <TodoSearch />
+      <input placeholder="Cebolla"></input>
+      
+      // contenedor de TODOs
       <TodoList>
-        <TodoItem />
+        {/* enviar cada uno de los todo que necesitamos */}
+        {/* iteramos los elementos del array todos */}
+        {todos.map(todo => (
+          <TodoItem />
+        ))}
       </TodoList>
+      
+      // boton que abre el modal para crear nuevos TODO
       <CreateTodoButton />
+      <button>+</button>
   );
 }
 
