ca73938b01656c9f9b3b396961652523f021fa54
mariokarajallo <mario_karajallo@hotmail.com>
2022-10-03 17:59:28 -0300

    create style TodoSearch.css

--- src/TodoSearch.jsx
+++ src/TodoSearch.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import "./TodoSearch.css";
 
 function TodoSearch() {
-  return <input placeholder="Cebolla"></input>;
+  return <input className="TodoSearch" placeholder="Cebolla"></input>;
 }
 
 export { TodoSearch };
