a21f369d7224e594dfc409eb151e50930adbdecd
Mario Karajallo <mariokarajallo92@gmail.com>
2022-10-24 18:17:16 -0300

    add style TodoIcon.css

--- src/TodoIcon/Index.js
+++ src/TodoIcon/Index.js
@@ -1,13 +1,24 @@
 import React from "react";
 import "./TodoIcon.css";
+import { ReactComponent as CheckSVG } from "./check.svg";
+import { ReactComponent as DeleteSVG } from "./delete.svg";
 
-function TodoIcon(type, color, onClick) {
+const iconTypes = {
+  check: (color) => (
+    <CheckSVG className="Icon-svg Icon-svg--check" fill={color} />
+  ),
+  delete: (color) => (
+    <DeleteSVG className="Icon-svg Icon-svg--delete" fill={color} />
+  ),
+};
+function TodoIcon(type, color = "gray", onClick) {
   return (
     <span
       className={`Icon-container Icon-container--${type}`}
       onClick={onClick}
     >
       {/* aqui deberia ir el icono formato SVG */}
+      {iconTypes[type]}
     </span>
   );
 }
